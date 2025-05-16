782bb603a42b07c3282d4032ac4ee9b0f631cbb5
pilarhl20@gmail.com <188579669+Pilar2094@users.noreply.github.com>
2025-05-16 15:09:18 +0200

    streamlit

--- src/streamlit_app copy.py
+++ src/streamlit_app copy.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+import joblib 
 import json
 
 # ========================
@@ -14,7 +14,7 @@ full_df = pd.read_csv("../data/processed/total_data_240k.csv")
 
 with open("../data/processed/Json/ciudad_transformation_rules.json") as f:
     ciudad_mapping = json.load(f)
-id_to_ciudad = {str(v): k for k, v in ciudad_mapping.items()}pip n
+id_to_ciudad = {str(v): k for k, v in ciudad_mapping.items()}pip
 
 # ========================
 # 2. Función para construir input del usuario
